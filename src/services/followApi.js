78fb2bf33ec4dd3f26974fdb6be4994cedfdfcb7
dldsudqlsdlqslek <tina4686@gmail.com>
2025-07-14 03:03:18 +0900

    Feat: API 불러오기

---
+++ src/services/followApi.js
@@ -0,0 +1,54 @@
+import apiClient from "./api";
+
+
+FOLLOW_API_BASE_URL = "api/v1";
+
+export const followUser = async (userId) => {
+    try {
+        const response = await apiClient.post(`${FOLLOW_API_BASE_URL}/users/${userId}/follow`);
+        return response.data;
+    } catch (error) {
+        console.error("❌ 팔로우 실패:", error);
+        throw error;
+    }
+}
+
+export const unfollowUser = async (userId) => {
+    try {
+        await apiClient.delete(`${FOLLOW_API_BASE_URL}/users/${userId}/follow`);
+    } catch (error) {
+        console.error("❌ 언팔로우 실패:", error);
+        throw error;
+    }
+}
+
+
+export const getFollowingList = async () => {
+    try {
+        const response = await apiClient.get(`${FOLLOW_API_BASE_URL}/users/following`);
+        return response.data;
+    } catch (error) {
+        console.error("❌ 팔로잉 목록 조회 실패:", error);
+        throw error;
+    }
+}
+
+export const getFollowerList = async () => {
+    try {
+        const response = await apiClient.get(`${FOLLOW_API_BASE_URL}/users/follower`);
+        return response.data;
+    } catch (error) {
+        console.error("❌ 팔로워 목록 조회 실패:", error);
+        throw error;
+    }
+}
+
+export const getFollowingPost = async () => {
+    try {
+        const response = await apiClient.get(`${FOLLOW_API_BASE_URL}/users/followPost`);
+        return response.data;
+    } catch (error) {
+        console.error("❌ 팔로잉 게시글 조회 실패:", error);
+        throw error;
+    }
+}
