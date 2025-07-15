7aa90b51bd91f9a2494518b2ef8374a5a0554824
s0___0k <61587396+s0ooo0k@users.noreply.github.com>
2025-07-15 12:46:41 +0900

    Fix: nav바에 이름 대신 닉네임 나오도록 수정

--- src/components/common/NavigationBar.jsx
+++ src/components/common/NavigationBar.jsx
@@ -107,7 +107,7 @@ export const NavigationBar = ({ onlyLogo }) => {
                   <span
                     style={{ fontWeight: 500, marginLeft: 4, marginRight: 8 }}
                   >
-                    {user?.name || "사용자"}
+                    {user?.nickname || "사용자"}
                   </span>
                   <Button
                     variant="outline-dark"
