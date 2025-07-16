e00e9f7e1a715f52b0c3f70043dd7287aac5626c
klaus9267 <klaus9267@gmail.com>
2025-07-16 14:32:43 +0900

    Feat: 알람 클릭 시 해당 페이지로 이동 구현현

--- src/pages/dm/DMPage.jsx
+++ src/pages/dm/DMPage.jsx
@@ -3,13 +3,16 @@ import { HeroSection } from "../../components/common/HeroSection";
 import { DMContainer } from "../../components/dm/DMContainer";
 import heroChatImage from "../../assets/hero-chat.png";
 import "../../styles/components/dm/dm.css";
+import { useLocation } from "react-router-dom";
 
 const DMPage = () => {
+  const location = useLocation();
+  const { roomId = null, messageId = null } = location.state || {};
   return (
     <>
       <HeroSection backgroundImageSrc={heroChatImage} />
       <div className="py-4">
-        <DMContainer />
+        <DMContainer initialRoomId={roomId} initialMessageId={messageId} />
       </div>
     </>
   );
